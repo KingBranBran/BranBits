9cce450feb1edb329530bdbb67161de3bc88ba6c
KingBranBran <kingbran4521@gmail.com>
2022-09-24 10:47:35 -0700

    Fix layout

---
+++ gui/src/main/java/me/branbran/branbits/App.java
@@ -0,0 +1,13 @@
+package me.branbran.branbits;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
